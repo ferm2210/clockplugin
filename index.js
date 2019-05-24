8a4a9babd77831e83a2df2f84f6426d8fa39624b
FERM <ferm2210@gmail.com>
2019-05-24 12:30:39 -0500

    terminado segundos y minutos y las correciones

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-function showTime(){
-   var date = new Date();
-   var h = date.getHours(); 
-   var m = date.getMinutes(); 
-   var s = date.getSeconds();
-   
-   h = (h < 10) ? "0" + h : h;
-   m = (m < 10) ? "0" + m : m;
-   s = (s < 10) ? "0" + s : s;
-   
-   var time = h + ":" + m + ":" + s ;
-   document.getElementById("MyClockDisplay").innerText = time;
-   
-   setTimeout(showTime, 1000);
-   
-   
+let hour = () => 
+   {  let h = new Date().getHours()
+      h = (h < 10) ? "0" + h : h
+      return h}
+
+let minutes = () => 
+   {  let m = new Date().getMinutes()
+      m = (m < 10) ? "0" + m : m
+      return m}
+
+let seconds = () => 
+   {  let s = new Date().getSeconds()
+      s = (s < 10) ? "0" + s : s
+      return s}
+
+let timeSec = () => `${hour()}:${minutes()}:${seconds()}`
+
+let timeMin = () => `${hour()}:${minutes()}`
+
+let printSec = (id) => document.getElementById(id).innerText = timeSec();
+let printMin = (id) => document.getElementById(id).innerText = timeMin();
+
+printSec("MyClockDisplay")
+setInterval(()=>printSec("MyClockDisplay"), 1000)
+
+printMin("MyClockDisplay1")
+if(seconds()=="00"){
+   setInterval(()=>printMin("MyClockDisplay1"), 60000)
+}else{
+   setTimeout(() => {
+      printMin("MyClockDisplay1")
+      setInterval(()=>printMin("MyClockDisplay1"), 60000)
+   }, ((60-parseInt(seconds()))*1000));
 }
 
-showTime();
 
